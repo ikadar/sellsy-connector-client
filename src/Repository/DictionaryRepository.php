d1d2cb1c602cd20084ea41c55f43453b08d799f7
Istvan Kadar <odesk.kadari@gmail.com>
2024-07-26 10:09:52 +0200

    Implement requests for /rate-categories and /taxes endpoints

---
+++ src/Repository/DictionaryRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace IKadar\SellsyConnectorClient\Repository;
+
+use Exception;
+use IKadar\Repository\Repository\Repository;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class DictionaryRepository extends Repository
+{
+
+    /**
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function getRateCategories($label = null, $page = 0, $limit = 10): ?array
+    {
+        return $this->executeQuery("getRateCategories", [
+            "label" => $label,
+            "page" => $page,
+            "limit" => $limit
+        ]);
+    }
+
+    /**
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function getRateCategoryByLabel($label = null, $page = 0, $limit = 10): ?array
+    {
+        return $this->executeQuery("getRateCategories", [
+            "label" => $label,
+            "page" => $page,
+            "limit" => $limit
+        ])["data"][0];
+    }
+
+    /**
+     * @throws Exception|TransportExceptionInterface
+     */
+    public function getTaxes($rate = null, $page = 0, $limit = 10): ?array
+    {
+        $response = $this->executeQuery("getTaxes", [
+            "rate" => $rate,
+            "page" => $page,
+            "limit" => $limit
+        ]);
+        return $response;
+    }
+
+    /**
+     * @throws Exception|TransportExceptionInterface
+     */
+    public function getTaxByLabel($rate = null, $page = 0, $limit = 10): ?array
+    {
+        $response = $this->executeQuery("getTaxes", [
+            "rate" => $rate,
+            "page" => $page,
+            "limit" => $limit
+        ]);
+        return $response["data"][0];
+    }
+
+}
---
+++ tests/DictionaryTest.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Tests;
+
+use IKadar\HTTPClient\Client\Client;
+use IKadar\HTTPClient\Connection\StaticOAuthConnection;
+use IKadar\HTTPClient\Request\RequestFactory;
+use IKadar\Repository\DataAccess\HTTPAPIClient;
+use IKadar\SellsyConnectorClient\Repository\DictionaryRepository;
+use IKadar\SellsyConnectorClient\SellsyConnectorAPIQueryBuilder;
+use PHPUnit\Framework\TestCase;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+
+class DictionaryTest extends TestCase
+{
+
+    private Client $client;
+    private DictionaryRepository $dictionaryRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // This value will come from tenant configuration
+        // It probably won't change by tenant,
+        // but better to store it there until the configuration implementation improvements.
+        $testApiRootUrl = "https://sellsy-connector.wheel.digital/api";
+        $testApiVersion = "v1";
+        $testApiClientId = "65bd1d2a73e19";
+        $testApiSecret = "EO8Dt2KTjuRGmOnHEWuJbg==";
+
+        // todo: $this->client can come from services.yaml as a parameter
+        $this->dictionaryRepository = new DictionaryRepository(
+            new HTTPAPIClient(new Client(
+                new StaticOAuthConnection(
+                    $testApiRootUrl,
+                    $testApiVersion,
+                    $testApiClientId,
+                    $testApiSecret,
+                )
+            )),
+            new SellsyConnectorAPIQueryBuilder(new RequestFactory())
+        );
+    }
+
+    public function testGetRateCategories()
+    {
+
+        dump(__METHOD__);
+
+        try {
+            $response = $this->dictionaryRepository->getRateCategories();
+        } catch (
+        ClientExceptionInterface|
+        RedirectionExceptionInterface|
+        ServerExceptionInterface|
+        TransportExceptionInterface
+        $e
+        ) {
+        }
+
+        dump($response);
+
+        $this->assertTrue(true); // Example assertion
+    }
+
+    public function testGetRateCategoryByLabel()
+    {
+
+        dump(__METHOD__);
+
+        $label = "HT";
+        try {
+            $response = $this->dictionaryRepository->getRateCategoryByLabel($label);
+        } catch (
+        ClientExceptionInterface|
+        RedirectionExceptionInterface|
+        ServerExceptionInterface|
+        TransportExceptionInterface
+        $e
+        ) {
+        }
+
+        dump($response);
+
+        $this->assertTrue(true); // Example assertion
+    }
+
+    public function testGetTaxes()
+    {
+
+        dump(__METHOD__);
+
+        try {
+            $response = $this->dictionaryRepository->getTaxes();
+        } catch (
+        ClientExceptionInterface|
+        RedirectionExceptionInterface|
+        ServerExceptionInterface|
+        TransportExceptionInterface
+        $e
+        ) {
+        }
+
+        dump($response);
+
+        $this->assertTrue(true); // Example assertion
+    }
+
+    public function testGetTaxByRate()
+    {
+
+        dump(__METHOD__);
+
+        $label = "20";
+        try {
+            $response = $this->dictionaryRepository->getTaxByLabel($label);
+        } catch (
+        ClientExceptionInterface|
+        RedirectionExceptionInterface|
+        ServerExceptionInterface|
+        TransportExceptionInterface
+        $e
+        ) {
+        }
+
+        dump($response);
+
+        $this->assertTrue(true); // Example assertion
+    }
+
+
+
+}
